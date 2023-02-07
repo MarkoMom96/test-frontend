c3fe1baab5583d72c5f5b054924edbab5990da73
Marko <MarkoMomcilovicMoca@gmail.com>
2023-02-08 00:49:40 +0100

    Implement the header component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+import Navigation from './components/Navigation'
 
 function App() {
 	const [count, setCount] = useState(0)
@@ -15,7 +16,11 @@ function App() {
 			.then(response => response.json())
 			.then(data => console.log(data))
 	}, [])
-	return <div className='w-9/12 mx-auto bg-slate-300 h-screen'></div>
+	return (
+		<div className='w-2/3 mx-auto'>
+			<Navigation />
+		</div>
+	)
 }
 
 export default App
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,55 @@
+const Navigation = () => {
+	return (
+		<header className='mt-7 p-2'>
+			<nav className='mb-9'>
+				<ul className='text-xs flex justify-end items-center'>
+					<li className='text-lg inline mr-auto'>
+						<img src='../../public/Logo.png' alt='LOGO' />
+					</li>
+					<li className='inline px-2 py-2 hover:bg-blue-200 transition-colors duration-200'>
+						<a href='#'>Lorem ipsum</a>
+					</li>
+					<li className='inline px-2 py-2 hover:bg-blue-200 transition-colors duration-200'>
+						<a href='#'>Lorem ipsum</a>
+					</li>
+					<li className='inline px-2 py-2 hover:bg-blue-200 transition-colors duration-200'>
+						<a href='#'>Lorem ipsum</a>
+					</li>
+					<li className='inline px-2 py-2 hover:bg-blue-200 transition-colors duration-200'>
+						<a href='#'>Lorem ipsum</a>
+					</li>
+				</ul>
+			</nav>
+			<nav>
+				<ul className=' ml-auto text-sm flex w-fit justify-end items-center border-blue-300 border-2'>
+					<li className='inline px-3 py-1 hover:bg-blue-200 transition-colors duration-200'>
+						<a href='#1'>Lorem ipsum</a>
+					</li>
+					<li className='inline px-3 py-1 hover:bg-blue-200 transition-colors duration-200'>
+						<a href='#2'>Lorem ipsum</a>
+					</li>
+					<li className='inline px-3 py-1 hover:bg-blue-200 transition-colors duration-200'>
+						<a href='#3'>Lorem ipsum</a>
+					</li>
+					<li className='inline px-3 py-1 hover:bg-blue-200 transition-colors duration-200'>
+						<a href='#'>Lorem ipsum</a>
+					</li>
+					<li className='inline px-3 py-1 hover:bg-blue-200 transition-colors duration-200'>
+						<a href='#'>Lorem ipsum</a>
+					</li>
+					<li className='inline px-3 py-1 hover:bg-blue-200 transition-colors duration-200'>
+						<a href='#'>Lorem ipsum</a>
+					</li>
+					<li className='inline px-3 py-1 hover:bg-blue-200 transition-colors duration-200'>
+						<a href='#'>Lorem ipsum</a>
+					</li>
+					<li className='inline px-3 py-1 hover:bg-blue-200 transition-colors duration-200'>
+						<a href='#'>Lorem ipsum</a>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	)
+}
+
+export default Navigation
